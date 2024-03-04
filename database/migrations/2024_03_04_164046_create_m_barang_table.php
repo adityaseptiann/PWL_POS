b2d4a29dac4dda92045b3a542f10250e8eea2045
adityaseptiann <dittsyn160@gmail.com>
2024-03-04 23:49:11 +0700

    prak 2.2

--- database/migrations/2024_03_04_164046_create_m_barang_table.php
+++ database/migrations/2024_03_04_164046_create_m_barang_table.php
@@ -4,23 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMBarangTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::create('m_barang', function (Blueprint $table) {
             $table->id('barang_id');
-            $table->string('barang_kode', 10)->unique();
+            $table->unsignedBigInteger('kategori_id')->index();
             $table->string('barang_nama', 100);
+            $table->integer('harga');
             $table->timestamps();
 
-            // mendefinisikan foreign key pada kolom level_id mengacu pada kolom level_id di table m_level
-            $table->foreign('kategoris_id')->references('kategoris_id')->on('m_kategoris');
+            $table->foreign('kategori_id')->references('kategori_id')->on('m_kategori');
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('m_barang');
     }
-}
+};
