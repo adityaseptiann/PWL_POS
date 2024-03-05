c5b35a878cd4c19ad25e4f359b3a8c9d7bd611fe
adityaseptiann <dittsyn160@gmail.com>
2024-03-05 10:48:29 +0700

    Jobsheet 4 Praktikum 1 - $fillable

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -12,7 +12,7 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @@foreach ($data as $d)
+            @foreach ($data as $d)
             <tr>
                 <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username }}</td>
